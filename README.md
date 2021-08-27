0e68bc4ed374ffcb872c36b90a84695b84dd5305
aproulx232 <aproulx@purdue.edu>
2021-08-27 10:49:04 -0400

    Update Readme

--- README.md
+++ README.md
@@ -1,19 +1,8 @@
-# TensorFlow Lite Python object detection example with Pi Camera
+# Cat Detecting Detergent
 
-This example uses [TensorFlow Lite](https://tensorflow.org/lite) with Python
-on a Raspberry Pi to perform real-time object detection using images
-streamed from the Pi Camera. It draws a bounding box around each detected
-object in the camera preview (when the object score is above a given threshold).
+This project uses a raspberry pi to detect when a cat is in a restricted area and deters them from entering this area.
 
-Although the TensorFlow model and nearly all the code in here can work with
-other hardware, the code uses the [`picamera`](
-https://picamera.readthedocs.io/en/latest/) API to capture images from the Pi
-Camera. So you can modify those parts of the code if you want to use a different
-camera input.
-
-At the end of this page, there are extra steps to accelerate the example using
-the Coral USB Accelerator, which increases the inference speed by ~10x.
-
+It is based off of the [Raspberry Pi TensorFlow Lite Object Detection Example](https://github.com/tensorflow/examples/tree/master/lite/examples/object_detection/raspberry_pi) and the [Video Streaming with Raspberry Pi Camera Example](https://randomnerdtutorials.com/video-streaming-with-raspberry-pi-camera/)
 
 ## Set up your hardware
 
@@ -23,11 +12,6 @@ Raspberry Pi OS (preferably updated to Buster).
 
 You also need to [connect and configure the Pi Camera](
 https://www.raspberrypi.org/documentation/configuration/camera.md).
-
-And to see the results from the camera, you need a monitor connected
-to the Raspberry Pi. It's okay if you're using SSH to access the Pi shell
-(you don't need to use a keyboard connected to the Pi)—you only need a monitor
-attached to the Pi to see the camera stream.
 
 
 ## Install the TensorFlow Lite runtime
@@ -46,15 +30,13 @@ Return here after you perform the `apt-get install` command.
 First, clone this Git repo onto your Raspberry Pi like this:
 
 ```
-git clone https://github.com/tensorflow/examples --depth 1
+git clone https://github.com/aproulx232/cat-cam.git
 ```
 
-Then use our script to install a couple Python packages, and
+Then use the script to install a couple Python packages, and
 download the MobileNet model and labels file:
 
 ```
-cd examples/lite/examples/object_detection/raspberry_pi
-
 # The script takes an argument specifying where you want to save the model files
 bash download.sh /tmp
 ```
@@ -63,82 +45,13 @@ bash download.sh /tmp
 ## Run the example
 
 ```
-python3 detect_picamera.py \
+python3 stream_object_detection.py \
   --model /tmp/detect.tflite \
   --labels /tmp/coco_labels.txt
 ```
 
-You should see the camera feed appear on the monitor attached to your Raspberry
-Pi. Put some objects in front of the camera, like a coffee mug or keyboard, and
-you'll see boxes drawn around those that the model recognizes, including the
-label and score for each. It also prints the amount of time it took
-to perform each inference in milliseconds at the top-left corner of the screen.
-
-For more information about executing inferences with TensorFlow Lite, read
-[TensorFlow Lite inference](https://www.tensorflow.org/lite/guide/inference).
-
-
-## Speed up the inferencing time (optional)
-
-If you want to significantly speed up the inference time, you can attach an
-ML accelerator such as the [Coral USB Accelerator](
-https://coral.withgoogle.com/products/accelerator)—a USB accessory that adds
-the [Edge TPU ML accelerator](https://coral.withgoogle.com/docs/edgetpu/faq/)
-to any Linux-based system.
-
-If you have a Coral USB Accelerator, follow these additional steps to
-delegate model execution to the Edge TPU processor:
-
-1.  First, be sure you have completed the [USB Accelerator setup instructions](
-    https://coral.withgoogle.com/docs/accelerator/get-started/).
-
-2.  Now open the `detect_picamera.py` file and add the following import at
-    the top:
-
-    ```
-    from tflite_runtime.interpreter import load_delegate
-    ```
-
-    And then find the line that initializes the `Interpreter`, which looks like
-    this:
-
-    ```
-    interpreter = Interpreter(args.model)
-    ```
-
-    And change it to specify the Edge TPU delegate:
-
-    ```
-    interpreter = Interpreter(args.model,
-        experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
-    ```
-
-    The `libedgetpu.so.1.0` file is provided by the Edge TPU library you
-    installed during the USB Accelerator setup in step 1.
-
-3.  Finally, you need a version of the model that's compiled for the Edge TPU.
-
-    Normally, you need to use use the [Edge TPU Compiler](
-    https://coral.withgoogle.com/docs/edgetpu/compiler/) to compile your
-    `.tflite` file. But the compiler tool isn't compatible with Raspberry
-    Pi, so we included a pre-compiled version of the model in the `download.sh`
-    script above.
-
-    So you already have the compiled model you need:
-    `mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite`.
-
-Now you're ready to execute the TensorFlow Lite model on the Edge TPU. Just run
-`detect_picamera.py` again, but be sure you specify the model that's compiled
-for the Edge TPU (it uses the same labels file as before):
-
+The camera feed with object detection bounding boxes should appear at the address 
 ```
-python3 detect_picamera.py \
-  --model /tmp/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite \
-  --labels /tmp/coco_labels.txt
+http://<your local network ip address>:8000/index.html
 ```
 
-You should see significantly faster inference speeds.
-
-For more information about creating and running TensorFlow Lite models with
-Coral devices, read [TensorFlow models on the Edge TPU](
-https://coral.withgoogle.com/docs/edgetpu/models-intro/).
--- stream_object_detection.py
+++ stream_object_detection.py
@@ -30,11 +30,11 @@ CAMERA_HEIGHT = 480
 
 def load_labels(path):
     """Loads the labels file. Supports files with or without index numbers."""
-    with open(path, 'r', encoding='utf-8') as f:
+    with open(path, "r", encoding="utf-8") as f:
         lines = f.readlines()
         labels = {}
         for row_number, content in enumerate(lines):
-            pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
+            pair = re.split(r"[:\s]+", content.strip(), maxsplit=1)
             if len(pair) == 2 and pair[0].strip().isdigit():
                 labels[int(pair[0])] = pair[1].strip()
             else:
@@ -44,7 +44,7 @@ def load_labels(path):
 
 def set_input_tensor(interpreter, image):
     """Sets the input tensor."""
-    tensor_index = interpreter.get_input_details()[0]['index']
+    tensor_index = interpreter.get_input_details()[0]["index"]
     input_tensor = interpreter.tensor(tensor_index)()[0]
     input_tensor[:, :] = image
 
@@ -52,7 +52,7 @@ def set_input_tensor(interpreter, image):
 def get_output_tensor(interpreter, index):
     """Returns the output tensor at the given index."""
     output_details = interpreter.get_output_details()[index]
-    tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
+    tensor = np.squeeze(interpreter.get_tensor(output_details["index"]))
     return tensor
 
 
@@ -71,9 +71,9 @@ def detect_objects(interpreter, image, threshold):
     for i in range(count):
         if scores[i] >= threshold:
             result = {
-                'bounding_box': boxes[i],
-                'class_id': classes[i],
-                'score': scores[i]
+                "bounding_box": boxes[i],
+                "class_id": classes[i],
+                "score": scores[i],
             }
             results.append(result)
     return results
@@ -84,16 +84,19 @@ def annotate_objects(image, results, labels):
     for obj in results:
         # Convert the bounding box figures from relative coordinates
         # to absolute coordinates based on the original resolution
-        ymin, xmin, ymax, xmax = obj['bounding_box']
+        ymin, xmin, ymax, xmax = obj["bounding_box"]
         xmin = int(xmin * CAMERA_WIDTH)
         xmax = int(xmax * CAMERA_WIDTH)
         ymin = int(ymin * CAMERA_HEIGHT)
         ymax = int(ymax * CAMERA_HEIGHT)
 
         draw = ImageDraw.Draw(image)
-        draw.rectangle([xmin, ymin, xmax, ymax], outline='Red')
-        draw.text([xmin, ymin],
-                  '%s\n%.2f' % (labels[obj['class_id']], obj['score']),  fill='Red')
+        draw.rectangle([xmin, ymin, xmax, ymax], outline="Red")
+        draw.text(
+            [xmin, ymin],
+            "%s\n%.2f" % (labels[obj["class_id"]], obj["score"]),
+            fill="Red",
+        )
 
 
 PAGE = """\
@@ -116,7 +119,7 @@ class StreamingOutput(object):
         self.condition = Condition()
 
     def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
+        if buf.startswith(b"\xff\xd8"):
             # New frame, copy the existing buffer's content and notify all
             # clients it's available
             # self.buffer.truncate()
@@ -129,24 +132,25 @@ class StreamingOutput(object):
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
+        if self.path == "/":
             self.send_response(301)
-            self.send_header('Location', '/index.html')
+            self.send_header("Location", "/index.html")
             self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
+        elif self.path == "/index.html":
+            content = PAGE.encode("utf-8")
             self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
+            self.send_header("Content-Type", "text/html")
+            self.send_header("Content-Length", len(content))
             self.end_headers()
             self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
+        elif self.path == "/stream.mjpg":
             self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
+            self.send_header("Age", 0)
+            self.send_header("Cache-Control", "no-cache, private")
+            self.send_header("Pragma", "no-cache")
             self.send_header(
-                'Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+                "Content-Type", "multipart/x-mixed-replace; boundary=FRAME"
+            )
             self.end_headers()
             try:
                 while True:
@@ -154,31 +158,33 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                         output.condition.wait()
                         frame = output.frame
 
-                        image = Image.open(output.buffer).convert('RGB').resize(
-                            (input_width, input_height), Image.ANTIALIAS)
+                        image = (
+                            Image.open(output.buffer)
+                            .convert("RGB")
+                            .resize((input_width, input_height), Image.ANTIALIAS)
+                        )
                         start_time = time.monotonic()
 
-                        results = detect_objects(
-                            interpreter, image, args.threshold)
+                        results = detect_objects(interpreter, image, args.threshold)
                         elapsed_ms = (time.monotonic() - start_time) * 1000
 
                         annotate_objects(image, results, labels)
                         img_byte_arr = io.BytesIO()
-                        image.save(img_byte_arr, format='jpeg')
+                        image.save(img_byte_arr, format="jpeg")
 
                         output.buffer.truncate()
-                        #frame = output.buffer.getvalue()
+                        # frame = output.buffer.getvalue()
                         frame = img_byte_arr.getvalue()
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
+                    self.wfile.write(b"--FRAME\r\n")
+                    self.send_header("Content-Type", "image/jpeg")
+                    self.send_header("Content-Length", len(frame))
                     self.end_headers()
                     self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
+                    self.wfile.write(b"\r\n")
             except Exception as e:
                 logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
+                    "Removed streaming client %s: %s", self.client_address, str(e)
+                )
         else:
             self.send_error(404)
             self.end_headers()
@@ -190,37 +196,31 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 
 # main
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--model", help="File path of .tflite file.", required=True)
+parser.add_argument("--labels", help="File path of labels file.", required=True)
 parser.add_argument(
-    '--model', help='File path of .tflite file.', required=True)
-parser.add_argument(
-    '--labels', help='File path of labels file.', required=True)
-parser.add_argument(
-    '--threshold',
-    help='Score threshold for detected objects.',
+    "--threshold",
+    help="Score threshold for detected objects.",
     required=False,
     type=float,
-    default=0.6)
+    default=0.6,
+)
 args = parser.parse_args()
 
 labels = load_labels(args.labels)
 interpreter = Interpreter(args.model)
 interpreter.allocate_tensors()
-_, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
+_, input_height, input_width, _ = interpreter.get_input_details()[0]["shape"]
 
 with picamera.PiCamera(resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=5) as camera:
     output = StreamingOutput()
     # Uncomment the next line to change your Pi's Camera rotation (in degrees)
     # camera.rotation = 90
-    camera.start_recording(output, format='mjpeg')
+    camera.start_recording(output, format="mjpeg")
     try:
-        address = ('', 8000)
+        address = ("", 8000)
         server = StreamingServer(address, StreamingHandler)
         server.serve_forever()
     finally:
         camera.stop_recording()
-
-
-# bash download.sh /tmp
-# sudo python3 stream_object_detection.py --model /tmp/detect.tflite --labels /tmp/coco_labels.txt
